0b86569a80ea8a44d2503f4b078072c0e16aef1c
Juan Castillo <jcastillo483@unab.edu.co>
2026-04-10 09:22:55 -0500

    Maquetacion Login/Register

--- app/src/main/java/com/example/exploraapp/MainActivity.kt
+++ app/src/main/java/com/example/exploraapp/MainActivity.kt
@@ -18,30 +18,9 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            ExploraAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+            LoginScreen(onLoginSuccess = {}, onNavigateToRegister = {})
                 }
             }
         }
-    }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ExploraAppTheme {
-        Greeting("Android")
-    }
-}

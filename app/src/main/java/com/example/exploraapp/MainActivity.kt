c27841ebec62e485f6f5e7c965603a4521b4461f
Juan Castillo <jcastillo483@unab.edu.co>
2026-04-10 09:38:15 -0500

    Configuracion de la navegacion

--- app/src/main/java/com/example/exploraapp/MainActivity.kt
+++ app/src/main/java/com/example/exploraapp/MainActivity.kt
@@ -11,16 +11,35 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.exploraapp.ui.theme.ExploraAppTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            LoginScreen(onLoginSuccess = {}, onNavigateToRegister = {})
+            val myNavController = rememberNavController()
+
+            NavHost(
+                navController = myNavController,
+                startDestination = "Login",
+                modifier = Modifier.fillMaxSize()
+            ) {
+                composable(route = "login") {
+                    LoginScreen(onLoginSuccess = {}, onNavigateToRegister = {})
                 }
+                composable(route = "register") {
+                    RegisterScreen(onRegisterSuccess = {}, onNavigateToLogin = {})
+                }
+
             }
+
         }
+    }
+}
 
 
